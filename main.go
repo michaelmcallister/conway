5a5de020a1e9c22bb3020656a0bc65a1fad5a753
michaelmcallister <me@michaelmcallister.org>
2020-07-28 21:31:48 +1000

    Some minor refactoring: - Threads because why not? I can't tell the difference, but they must make it faster because...threads - Just embed the Life struct into the struct that implments the ebiten.Game interface to make it a bit neater.

--- main.go
+++ main.go
@@ -1,52 +1,59 @@
 package main
 
 import (
+	"sync"
+
 	"github.com/hajimehoshi/ebiten"
 	"github.com/michaelmcallister/conway/life"
 )
 
+var waitGroup sync.WaitGroup
+
 const width, height = 320, 240
 
-// Game satisfies the ebiten.Game interface.
-type Game struct {
-	life *life.Life
+// conway satisfies the ebiten.Game interface.
+type conway struct {
+	*life.Life
 }
 
 // Update advances Life by one iteration.
-func (g *Game) Update(screen *ebiten.Image) error {
-	g.life.Step()
+func (c *conway) Update(screen *ebiten.Image) error {
+	c.Step()
 	return nil
 }
 
 // Draw takes the current board state and sets each alive cell to 0xFF (black).
-func (g *Game) Draw(screen *ebiten.Image) {
+func (c *conway) Draw(screen *ebiten.Image) {
 	pix := make([]byte, width*height*4)
-	for i, v := range g.life.BoardState() {
-		if v {
-			pix[4*i] = 0
-			pix[4*i+1] = 0
-			pix[4*i+2] = 0
-			pix[4*i+3] = 0
-		} else {
-			pix[4*i] = 0xff
-			pix[4*i+1] = 0xff
-			pix[4*i+2] = 0xff
-			pix[4*i+3] = 0xff
-		}
+	for i, v := range c.BoardState() {
+		waitGroup.Add(1)
+		go func(i int, v bool) {
+			defer waitGroup.Done()
+			if v {
+				pix[4*i] = 0
+				pix[4*i+1] = 0
+				pix[4*i+2] = 0
+				pix[4*i+3] = 0
+			} else {
+				pix[4*i] = 0xff
+				pix[4*i+1] = 0xff
+				pix[4*i+2] = 0xff
+				pix[4*i+3] = 0xff
+			}
+		}(i, v)
 	}
+	waitGroup.Wait()
 	screen.ReplacePixels(pix)
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
-func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+func (*conway) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return width, height
 }
 
 func main() {
-	life := life.New(width, height)
-
 	ebiten.SetWindowTitle("Conways Game Of Life")
-	game := &Game{life: life}
+	game := &conway{life.New(width, height)}
 	if err := ebiten.RunGame(game); err != nil {
 		panic(err)
 	}
